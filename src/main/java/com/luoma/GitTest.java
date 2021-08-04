aec0076a448ba504e482e3a6329331690ce5d686
luomashijie <luomashijie@outlook.com>
2021-08-04 20:26:37 +0800

    second commit

--- src/main/java/com/luoma/GitTest.java
+++ src/main/java/com/luoma/GitTest.java
@@ -2,7 +2,7 @@ package com.luoma;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git test");
-        System.out.println("ok");
+        System.out.println("git test1");
+        System.out.println("git test2");
     }
 }
