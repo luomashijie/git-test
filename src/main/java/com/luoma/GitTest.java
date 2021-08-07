151239031a996fa12189807df3687f268785b7c3
luomashijie <1832968964@qq.com>
2021-08-07 11:30:19 +0800

    pull commit

--- src/main/java/com/luoma/GitTest.java
+++ src/main/java/com/luoma/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("ssh test");
         System.out.println("pull test");
         System.out.println("pull test");
+        System.out.println("pull test");
     }
 }
