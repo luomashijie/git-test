a2551c73318b092a44c8e07d3919f3760f8c1c2f
luomashijie <luomashijie@outlook.com>
2021-08-07 15:06:25 +0800

    git push

--- src/main/java/com/luoma/GitTest.java
+++ src/main/java/com/luoma/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("pull test");
         System.out.println("pull test");
+        System.out.println("pull test");
     }
 }
